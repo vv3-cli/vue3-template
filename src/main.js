c8abc7a49d1a14d158a3ec5bfb9c955d74284808
liujunjie <864629630@qq.com>
2022-08-23 14:33:20 +0800

    feat: jjj

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
-import "./style.css";
-// import './views/index/indextest.scss'
+import "./assets/css/reset.css";
 import App from "./App.vue";
 import router from "./router/index";
 createApp(App).use(router).mount("#app");
--- src/request/methods.js
+++ src/request/methods.js
@@ -1,11 +1,5 @@
 import instance from "./index";
-/**
- * @param {String} method  请求的方法：get、post、delete、put
- * @param {String} url     请求的url:
- * @param {Object} data    请求的参数
- * @param {Object} config  请求的配置
- * @returns {Promise}     返回一个promise对象，其实就相当于axios请求数据的返回值
- */
+
 const axios = {
     get(url, params) {
         return new Promise((resolve, reject) => {
